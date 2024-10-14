ec4fa8fb29ce4b6cb07cde7819708e052ce5895c
Ramon Del Valle <ramon.dev@icloud.com>
2024-10-13 18:15:51 -0600

    Add CssClass property to BlazorIconBase
    Introduced the CssClass property to the BlazorIconBase class for better CSS class handling. Updated BlazorIcon.razor to use CssClass instead of Class to dynamically combine BaseClass and Class.

--- BlazorIcon/BlazorIconBase.cs
+++ BlazorIcon/BlazorIconBase.cs
@@ -4,6 +4,8 @@ namespace BlazorIcon;
 
 public abstract class BlazorIconBase : ComponentBase
 {
+    protected string? CssClass => string.IsNullOrWhiteSpace(BaseClass) ? Class : $"{BaseClass} {Class}";
+    protected virtual string? BaseClass { get; }
     [Parameter] public string? Class { get; set; }
     [Parameter] public string? Fill { get; set; }
     [Parameter] public string? ViewBox { get; set; }
