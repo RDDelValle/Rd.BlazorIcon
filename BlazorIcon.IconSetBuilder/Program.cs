db1e6a75aa4ae744a7fc8429c039dff80822598a
Ramon Del Valle <ramon.dev@icloud.com>
2024-10-13 19:21:58 -0600

    Add DocsBlockBuilder and enhance NPM script setup
    Introduced a new DocsBlockBuilder class to generate documentation comments in `BlazorIcon.IconSetBuilder`. Additionally, updated `package.json` and `package-lock.json` to include `nodemon` for better development workflow and added new NPM scripts for watching and starting the project.

--- BlazorIcon.IconSetBuilder/Program.cs
+++ BlazorIcon.IconSetBuilder/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Rd.BlazorIcon.IconSetBuilder.Utilities;
+
+var value = new DocsBlockBuilder
+{
+    VendorSource = "npm/bootstrap-icons/icons/",
+    VendorName = "Bootstrap Icons v1.11.3 (https://icons.getbootstrap.com/)",
+    VendorCopyright = "Copyright 2019-2024 The Bootstrap Authors",
+    VendorLicense = "Licensed under MIT (https://github.com/twbs/icons/blob/main/LICENSE)",
+};
+
+Console.WriteLine(value.ToString());
