7f3910480190a5355a23d55a57afdea917f3ca4b
Ramon Del Valle <ramon.dev@icloud.com>
2024-10-16 13:39:12 -0600

    Add BootstrapIcons class with SVG data
    Introduced a new `BootstrapIcons` class in the `BlazorIcon.Bootstrap` namespace. This class contains constant strings representing various SVG icons from Bootstrap Icons v1.11.3.

---
+++ BlazorIcon.IconSetBuilder/BootstrapIconSetBuilder.cs
@@ -0,0 +1,37 @@
+namespace Rd.BlazorIcon.IconSetBuilder;
+
+public class BootstrapIconSetBuilder : IconSetFileBuilder
+{
+    public string SvgIconsDirectory { get; set; } = "../node_modules/bootstrap-icons/icons";
+    public override string OutputDirectory { get; init; } = "../BlazorIcon.Bootstrap";
+    public override string OutputFilename { get; init; } = "BootstrapIcons";
+
+    public override IconSetClassBuilder IconSetClassBuilder { get; init; } = new IconSetClassBuilder
+    {
+        ProjectRepo = "https://github.com/RDDelValle/Rd.BlazorIcon",
+        Namespace = "Rd.BlazorIcon.Bootstrap",
+        PrimaryClass = "BootstrapIcons",
+        VendorName = "Bootstrap Icons v1.11.3 (https://icons.getbootstrap.com/)",
+        VendorSource = "node_modules/bootstrap-icons/icons/(https://github.com/twbs/icons/)",
+        VendorLicense = "Licensed under MIT (https://github.com/twbs/icons/blob/main/LICENSE)",
+        VendorCopyright = "Copyright 2019-2024 The Bootstrap Authors",
+    };
+    
+    public override string[]? WhiteListIcons { get; init; }
+    protected override void BuildIconSet()
+    {
+        foreach (var file in GetSvgFilesFromDirectory(SvgIconsDirectory))
+        {
+            if(WhiteListIcons != null && !WhiteListIcons.Contains(file.Split("/").Last().Split(".").First()))
+                continue;
+            var svgName = file.ToCSharpPropertyName();
+            var svg = ReadSvgFile(file);
+            svg.Attribute("class")?.Remove();
+            svg.Attribute("fill")?.Remove();
+            svg.Attribute("width")?.Remove();
+            svg.Attribute("height")?.Remove();
+            var formattedSvg = svg.ToString().Replace("\r", "").Replace("\n", "").Replace("\"", "\"\"").Replace(">  <", "><");
+            IconSetClassBuilder.AddIcon(svgName, formattedSvg);
+        }
+    }
+}
--- BlazorIcon.IconSetBuilder/Program.cs
+++ BlazorIcon.IconSetBuilder/Program.cs
@@ -1,8 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Rd.BlazorIcon.IconSetBuilder.Builders;
 
-BootstrapIconsBuilder.Build();
+using System.Text.RegularExpressions;
+using Rd.BlazorIcon.IconSetBuilder;
+
+// new BootstrapIconSetBuilder().WriteToFile(false, true);
 
 
+string svgString = @"<svg xmlns=""http://www.w3.org/2000/svg"" viewBox=""0 0 16 16""><path d=""M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm8.5 0v8H15V2zm0 9v3H15v-3zm-1-9H1v3h6.5zM1 14h6.5V6H1z"" /></svg>";
 
+// Define the regular expression pattern to match the opening and closing svg tags
+string openingTagPattern = @"<svg\b[^>]*>";
+string closingTagPattern = @"<\/svg>";
 
+// Remove the opening svg tag
+svgString = Regex.Replace(svgString, openingTagPattern, "", RegexOptions.IgnoreCase);
+
+// Remove the closing svg tag
+svgString = Regex.Replace(svgString, closingTagPattern, "", RegexOptions.IgnoreCase);
+
+Console.WriteLine("Result string: " + svgString);
