9eec5b3eb4e31af989895de104eeffd1843dfb6e
Ramon Del Valle <ramon.dev@icloud.com>
2024-10-16 13:40:01 -0600

    Replace BootstrapIcon with BlazorIcon
    Removed BootstrapIcon component and its dependencies. Updated references to use the BlazorIcon component throughout the project. Simplified the BlazorIconBase class for improved functionality and maintainability.

--- BlazorIcon/BlazorIconBase.cs
+++ BlazorIcon/BlazorIconBase.cs
@@ -1,13 +1,28 @@
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Components;
 
 namespace Rd.BlazorIcon;
 
 public abstract class BlazorIconBase : ComponentBase
 {
-    protected string? CssClass => string.IsNullOrWhiteSpace(BaseClass) ? Class : $"{BaseClass} {Class}";
-    protected virtual string? BaseClass { get; }
-    [Parameter] public string? Class { get; set; }
-    [Parameter] public string? ViewBox { get; set; } = "0 0 16 16";
+    protected string? FormattedIcon
+        => string.IsNullOrWhiteSpace(Icon)
+            ? null
+            : Icon.Replace(@"<svg\b[^>]*>", string.Empty)
+                .Replace(@"<\/svg>", string.Empty);
+
+    protected string? ViewBox
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(Icon))
+                return null;
+            var pattern = "viewBox=\"([^\"]+)\"";
+            var match = Regex.Match(Icon, pattern);
+            return match.Success ? match.Groups[1].Value : null;
+        }
+    }
+
     [Parameter] public string? Title { get; set; }
     [Parameter] public string? Icon { get; set; }
     [Parameter] public bool AriaHidden { get; set; } = true;
