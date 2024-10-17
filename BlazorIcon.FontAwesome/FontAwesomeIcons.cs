931329bfec9794182a3d14f0383e02b08d14cd6f
Ramon Del Valle <ramon.dev@icloud.com>
2024-10-17 02:45:45 -0600

    Add Font Awesome Brand icons to BlazorIcon.FontAwesome
    Introduced new icons from Font Awesome Free 6.6.0 for brands like Twitter, Facebook, and Bootstrap. Added necessary using directives and namespace configurations.

---
+++ BlazorIcon.FontAwesome/FontAwesomeIcons.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Rd.BlazorIcon.FontAwesome
+{
+   [ExcludeFromCodeCoverage]
+   public partial class FontAwesomeIcons
+    {
+       public partial class Regular
+        {
+        }
+       public partial class Solid
+        {
+        }
+       public partial class Brands
+        {
+        }
+    }
+}
