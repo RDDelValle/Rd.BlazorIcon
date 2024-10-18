dec8e78cdb1f70a3e8f3416d4a748af2307a0330
Ramon Del Valle <ramon.dev@icloud.com>
2024-10-18 12:40:53 -0600

    Remove demo app from repo.

--- BlazorIcon.Demo/AppComponentBase.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace Rd.BlazorIcon.Demo;
-
-public abstract class AppComponentBase : ComponentBase
-{
-    [CascadingParameter] public IApp App { get; set; } = default!;
-}
