e2601342d059e91adcb75ff50ab0cf3a359e7c45
Ramon DelValle <ramon.dev@icloud.com>
2024-10-18 19:43:45 -0600

    Setup New Demo Project

---
+++ BlazorIcon.Demo/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Rd.BlazorIcon.Demo;
+
+public static class UseBlazorIconDemo {
+    public static IServiceCollection AddBlazorIconDemo(this IServiceCollection services)
+    {
+        return services;
+    }
+}
