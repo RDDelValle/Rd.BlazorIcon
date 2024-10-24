5c0c831a003ba47d2485370f3d4564ecbf9cd88e
Ramon DelValle <ramon.dev@icloud.com>
2024-10-24 01:20:22 -0600

    Add BlazorIconSize enum and dynamic CSS class building
    Introduced BlazorIconSize enumeration to represent various icon sizes. Enhanced BlazorIconBase to build dynamic CSS classes based on size, fixed width, and additional attributes. Updated BlazorIcon.razor and BlazorIcon.razor.css to utilize these changes, enabling flexible and scalable icon styling.

--- BlazorIcon/BlazorIconBase.cs
+++ BlazorIcon/BlazorIconBase.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Components;
 
@@ -11,7 +12,7 @@ public abstract class BlazorIconBase : ComponentBase
         {
             if (string.IsNullOrWhiteSpace(Icon))
                 return null;
-            
+
             string openingTagPattern = @"<svg\b[^>]*>";
             string closingTagPattern = @"<\/svg>";
 
@@ -36,8 +37,30 @@ public abstract class BlazorIconBase : ComponentBase
         }
     }
 
+
+    protected string? CssClass
+        => BuildCssClass();
+
+    [Parameter] public string BaseClass { get; set; } = "blazor-icon";
+    [Parameter] public string? Class { get; set; }
     [Parameter] public string? Title { get; set; }
     [Parameter] public string? Icon { get; set; }
+    [Parameter] public BlazorIconSize Size { get; set; }
+    [Parameter] public bool IsFixedWidth { get; set; }
     [Parameter] public bool AriaHidden { get; set; } = true;
     [Parameter] public bool Focusable { get; set; }
+    [Parameter] public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    protected virtual string BuildCssClass()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"{BaseClass} ");
+        if (IsFixedWidth)
+            sb.Append("is-fw ");
+        if (Size != BlazorIconSize.Default)
+            sb.Append($"is-{Size.ToString()!.Replace("_", string.Empty).ToLower()} ");
+        if (!string.IsNullOrWhiteSpace(Class))
+            sb.Append($"{Class} ");
+        return sb.ToString().TrimEnd(' ');
+    }
 }
---
+++ BlazorIcon/BlazorIconSize.cs
@@ -0,0 +1,20 @@
+namespace Rd.BlazorIcon;
+
+public enum BlazorIconSize {
+    Default = 0,
+    _1x = 1,
+    _2x = 2,
+    _3x = 3,
+    _4x = 4,
+    _5x = 5,
+    _6x = 6,
+    _7x = 7,
+    _8x = 8,
+    _9x = 9,
+    _10x = 10,
+    _2xs = 11,
+    _Xs = 12,
+    _Lg = 13,
+    _Xl = 14,
+    _2xl = 15,
+}
