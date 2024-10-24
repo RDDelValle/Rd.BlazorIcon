83bd2c4f381c4ac7de060d2303fadeb63501cfa9
Ramon DelValle <ramon.dev@icloud.com>
2024-10-24 01:18:58 -0600

    Refactor and modularize component controls and lists
    Reorganized component code by modularizing the controls and lists. Introduced new components for demo controls and list items, replacing the previous single-file implementations. Improved maintainability and separation of concerns for better scalability.

--- BlazorIcon.Demo/Managers/DemoIconManager.cs
+++ BlazorIcon.Demo/Managers/DemoIconManager.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.Options;
 using Rd.BlazorIcon.Demo.Options;
 
@@ -5,23 +6,79 @@ namespace Rd.BlazorIcon.Demo.Managers;
 
 public class DemoIconManager(IOptions<DemoOptions> options)
 {
-    public IEnumerable<DemoIconSet> IconSets => options.Value.DemoIconSets;
-
-    private EventHandler? _onSelectedIconSetChange;
-    public event EventHandler OnSelectedIconSetChange
+    public FieldInfo? SelectedIcon { get; private set; }
+    private EventHandler? _onSelectedIconChange;
+    public event EventHandler OnSelectedIconChange
     {
-        add => _onSelectedIconSetChange += value;
-        remove => _onSelectedIconSetChange -= value;
+        add => _onSelectedIconChange += value;
+        remove => _onSelectedIconChange -= value;
     }
+
+    private EventHandler? _onIconSetChange;
+    public event EventHandler OnIconSetChange
+    {
+        add => _onIconSetChange += value;
+        remove => _onIconSetChange -= value;
+    }
+
+    public string? FilterString { get; internal set; }
+    public int IconSize { get; internal set; } = 3;
+    public IEnumerable<DemoIconSet> IconSets => options.Value.DemoIconSets;
 
     public DemoIconSet SelectedIconSet { get; private set; } = options.Value.DemoIconSets.First();
 
-    public void SelectIconSetAsync(DemoIconSet iconSet)
-    {   
-        if(iconSet == SelectedIconSet)
+
+    public IReadOnlyList<FieldInfo> FilteredIcons =>
+        string.IsNullOrWhiteSpace(FilterString)
+            ? Icons
+            : Icons.Where(e =>
+                    e.Name.Contains(FilterString, StringComparison.CurrentCultureIgnoreCase))
+                .ToList();
+
+    public List<FieldInfo> Icons
+        => SelectedIconSet
+            .Type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+            .Where(fi => fi is { IsLiteral: true, IsInitOnly: false }).ToList();
+    
+    public void SelectIcon(FieldInfo icon)
+    {
+        if (icon == SelectedIcon)
+            return;
+        SelectedIcon = icon;
+        _onSelectedIconChange?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void DeselectIcon()
+    {
+        if (SelectedIcon is null)
+            return;
+        SelectedIcon = null;
+        _onSelectedIconChange?.Invoke(this, EventArgs.Empty);
+    }
+
+
+    public void SelectIconSet(DemoIconSet iconSet)
+    {
+        if (iconSet == SelectedIconSet)
             return;
         SelectedIconSet = iconSet;
-        _onSelectedIconSetChange?.Invoke( this, EventArgs.Empty );
+        _onIconSetChange?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void FilterIconSet(string filterString)
+    {
+        if (filterString.Equals(FilterString, StringComparison.CurrentCultureIgnoreCase))
+            return;
+        FilterString = filterString;
+        _onIconSetChange?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void SizeIconSet(int size)
+    {
+        if (size == IconSize)
+            return;
+        IconSize = size;
+        _onIconSetChange?.Invoke(this, EventArgs.Empty);
     }
 
 }
